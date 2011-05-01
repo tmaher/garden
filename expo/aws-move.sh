88892ab6208cdf407d43dd8d43aa6515774406da
Tom Maher <tmaher@tursom.org>
2011-04-30 20:41:08 -0700

    handle the navbar, too

--- expo/aws-move.sh
+++ expo/aws-move.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-find . -name \*.htm\* -print0 | xargs -0 -n 1 -I ___ perl -i -pne 's,(["\(])imagebucket/,$1http://s3.amazonaws.com/expomuseum-imagebucket/,g; s,http://www.expomuseum.com/imagebucket/,http://s3.amazonaws.com/expomuseum-imagebucket/,g;'  ___
+find . -name \*.htm\* -print0 | xargs -0 -n 1 -I ___ perl -i -pne 's,(["\(])imagebucket/,$1http://s3.amazonaws.com/expomuseum-imagebucket/,g;
+
+s,http://www.expomuseum.com/imagebucket/,http://s3.amazonaws.com/expomuseum-imagebucket/,ig;
+
+s,(["\(])navbar/,$1http://s3.amazonaws.com/expomuseum-navbar/,g;
+s,http://www.expomuseum.com/navbar/,http://s3.amazonaws.com/expomuseum-navbar/,ig;'  ___
