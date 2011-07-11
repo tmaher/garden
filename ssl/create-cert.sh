19fcb194f7937923afb0107b777c5fd1d272460c
Tom Maher <tmaher@tursom.org>
2011-07-10 18:51:42 -0700

    Add support for subjectAltName certs.

--- ssl/create-cert.sh
+++ ssl/create-cert.sh
@@ -1,28 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
-CFGFILE="../CA/openssl.cnf"
+set -o errexit
+umask 0077
+
+cd `dirname $0` && SSLDIR="$PWD" && cd - >/dev/null
+CFGFILE="$SSLDIR/scoped-CA/openssl.cnf"
 CFG="-config $CFGFILE"
 
-SSLDIR=`dirname $0`
-cd ${SSLDIR}/hosts
+cd "$HOME/private/ssl/hosts"
 
 if [ ! -r $CFGFILE ]; then
   echo "can't find config file $CFGFILE under $SSLDIR"
   exit 1
 fi
 
-if [ x$1 == "x" ]; then
-  echo "usage: $0 fqdn"
+if  [ x$1 == "x-h" ] || [ x$1 == "x--h" ] || [ x$1 == "x--help" ] || \
+    [ $# -eq 0 ]; then
+  echo "usage: $0 fqdn [fqdn2 [fqdn3 [fqdn4 ...]]]"
   exit 1
 fi
 
-fqdn=$1
-if [ -r ${fqdn}* ]; then
-  echo "${fqdn}* exists! aborting"
+export RECFQDN="$1"
+
+while [ $# -gt 0 ]; do
+  _SAN="${_SAN},DNS:$1"
+  shift
+done
+export SUBJECTALTNAME=${_SAN/#,/}
+
+if [ -r ${RECFQDN}.key ]; then
+  echo "${RECFQDN}.key exists! aborting"
   exit 1
 fi
 
-openssl req ${CFG} -nodes -new -newkey rsa:2048 -keyout ${fqdn}.key -out ${fqdn}.csr
-openssl ca ${CFG} -in ${fqdn}.csr -out ${fqdn}.crt
+openssl req ${CFG} -nodes -new -newkey rsa:2048 \
+    -keyout ${RECFQDN}.key -out ${RECFQDN}.csr
+openssl ca ${CFG} -in ${RECFQDN}.csr -out ${RECFQDN}.crt
 
-ls -l ${fqdn}.*
+ls -l ${RECFQDN}.*
