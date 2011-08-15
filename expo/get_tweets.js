22827a45367bd8d5a151b4840d94413646f01f36
Tom Maher <tmaher@tursom.org>
2011-08-15 00:28:36 -0700

    simplify Date() handling; clean up namespace a bit

--- expo/get_tweets.js
+++ expo/get_tweets.js
@@ -2,15 +2,6 @@
 
 var https = require('https');
 
-var screen_name="ExpoMuseum";
-
-var base_path = "/1/statuses/user_timeline.json" +
-    "?screen_name=" + screen_name +
-    "&trim_user=true" +
-    "&include_rts=false";
-
-var options = { host: 'api.twitter.com' };
-
 var digest_tweets = function (t){
     var creation_out = new Date(Date.parse(t["created_at"])).toString();
     var out = creation_out + "\n" + t["text"] + "\n\n";
@@ -24,11 +15,16 @@ function process_tweets(all_tweets){
     }
 }
 
-function get_all_since_limit(limit) {
+function get_all_since_limit(screen_name, limit) {
     var all_tweets = [];
 
     var maxfetch = 6;
     var currfetch = 0;
+    var options = { host: 'api.twitter.com' };
+    var base_path = "/1/statuses/user_timeline.json" +
+        "?screen_name=" + screen_name +
+        "&trim_user=true" +
+        "&include_rts=false";
 
     function get_all_helper(currpage, limit){
         options["path"] = base_path + "&page=" + currpage;
@@ -42,8 +38,7 @@ function get_all_since_limit(limit) {
                 if(these_tweets.length < 1){ return; }
 
                 var oldest_tweet = these_tweets.pop();
-                var oldest_ts =
-                    new Date(Date.parse(oldest_tweet["created_at"])).getTime();
+                var oldest_ts = Date.parse(oldest_tweet["created_at"]);
                 these_tweets.push(oldest_tweet);
                 if((oldest_ts > limit) &&
                    (these_tweets.length > 0) &&
@@ -57,9 +52,11 @@ function get_all_since_limit(limit) {
                     var is_oldest;
                     do {
                         is_oldest = these_tweets.pop();
-                        o_ts = new Date(Date.parse(is_oldest["created_at"])).getTime();
+                        o_ts = Date.parse(is_oldest["created_at"]);
                     } while(o_ts <= limit && these_tweets.length >= 1);
+
                     if(o_ts > limit){ these_tweets.push(is_oldest); }
+
                     all_tweets = all_tweets.concat(these_tweets);
                     process_tweets(all_tweets);
                 }
@@ -76,4 +73,4 @@ function get_all_since_limit(limit) {
 }
 
 var oldest_ts_for_me = new Date().getTime() - (86400 * 1000);
-get_all_since_limit(oldest_ts_for_me);
+get_all_since_limit("ExpoMuseum", oldest_ts_for_me);
