b39a99b5dcda7f1cff98fc52b898dbce7f3475b7
Tom Maher <tmaher@tursom.org>
2011-09-03 19:14:31 -0700

    lingering S3 asset move cleanup for expo museum

--- expo/aws-move.sh
+++ expo/aws-move.sh
@@ -5,4 +5,13 @@ find . -name \*.htm\* -print0 | xargs -0 -n 1 -I ___ perl -i -pne 's,(["\(])imag
 s,http://www.expomuseum.com/imagebucket/,http://s3.amazonaws.com/expomuseum-imagebucket/,ig;
 
 s,(["\(])navbar/,$1http://s3.amazonaws.com/expomuseum-navbar/,g;
-s,http://www.expomuseum.com/navbar/,http://s3.amazonaws.com/expomuseum-navbar/,ig;'  ___
+s,http://www.expomuseum.com/navbar/,http://s3.amazonaws.com/expomuseum-navbar/,ig;
+
+s,(["\(])[\./]+imagebucket/,$1http://s3.amazonaws.com/expomuseum-imagebucket/,g;
+s,(["\(])[\./]+navbar/,$1http://s3.amazonaws.com/expomuseum-navbar/,g;
+
+s,http://www.expomuseum.com/[\./]+imagebucket/,http://s3.amazonaws.com/expomuseum-imagebucket/,ig;
+s,http://www.expomuseum.com/[\./]+navbar/,http://s3.amazonaws.com/expomuseum-navbar/,ig;
+
+
+'  ___
