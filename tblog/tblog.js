3d50532a071801b9ab62a81fe7884b222a8c8276
Tom Maher <tmaher@tursom.org>
2011-09-03 19:15:15 -0700

    lingering stuff from twitter-to-blogger processing

--- tblog/tblog.js
+++ tblog/tblog.js
@@ -1,3 +1,30 @@
 #!/usr/local/bin/node
 
-var h = require('https');
+var https = require('https');
+var sys = require('sys');
+var fs = require('fs');
+var oauth = require('oauth');
+
+var opt = {
+    // https://api.twitter.com/1/statuses/user_timeline.json?user_id=34335782
+    host: "api.twitter.com",
+    path: '/1/statuses/user_timeline.json?user_id=34335782&include_rts=false',
+};
+
+function do_req(p){
+    p.forEach(function (t){
+        console.log("«" + t.created_at + "; " + t.id + "»\n" + t.text + "\n\n");
+    });
+    process.stdout.write("\n");
+}
+
+var req = https.get(opt, function(res){
+    //console.log("statusCode: ", res.statusCode);
+    // console.log("headers: ", res.headers);
+
+    var payload = "";
+    res.on('data', function(d) { payload += d; });
+    res.on('end', function(){ do_req(JSON.parse(payload)); });
+});
+req.end();
+
