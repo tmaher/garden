f77d15009e326704e3b5e57cf39e2871ff850092
Tom Maher <tmaher@pw0n.me>
2015-01-10 23:21:25 -0800

    wip

--- audio/fetch-dead.rb
+++ audio/fetch-dead.rb
@@ -11,7 +11,7 @@ resp = Excon.get searchurl
 CSV.parse(resp.body) do |rec|
   next if all_shows.key? rec[0]
   all_shows[rec[0]] = {:date => rec[0], :id => rec[1], :pubdate => rec[2],
-    :title => rec[3].sub(/\AGrateful Dead Live at /, "").sub(/ on 19\d\d-\d\d-\d\d\z/,""),
+    :title => rec[3].sub(/\A(The |)Grateful Dead Live at /, "").sub(/ on 19\d\d-\d\d-\d\d\z/,""),
     :year => rec[4]
     }
 end
@@ -21,12 +21,13 @@ all_shows.delete("date")
 all_shows.each do |date,show|
   m3u_url = "https://archive.org/download/#{show[:id]}/#{show[:id]}_vbr.m3u"
   filename = "#{show[:year]}/#{date.sub(/T00:00:00Z\z/,'')} #{show[:title]}.m3u"
-  resp = Excon.get(m3u_url, {
+  unless File.exist?(filename)
+    resp = Excon.get(m3u_url, {
     middlewares: Excon.defaults[:middlewares] +
       [Excon::Middleware::RedirectFollower]
-    }
-  )
-  Dir.exist? show[:year] or Dir.mkdir show[:year]
-  File.open(filename, 'w') {|f| f.write(resp.body) }
-  puts "#{filename}"
+    })
+    Dir.exist? show[:year] or Dir.mkdir show[:year]
+    File.open(filename, 'w') {|f| f.write(resp.body) }
+    puts "#{filename}"
+  end
 end
